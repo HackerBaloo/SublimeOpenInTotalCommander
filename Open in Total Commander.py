ed46ee67c7a5f4f53a6990152205721bd978feb9
Björn Carlsson <b.r.carlsson@gmail.com>
2013-05-23 19:38:23 +0200

    Made changes to handle 64-bit Total Commander out of the boxx

--- Open in Total Commander.py
+++ Open in Total Commander.py
@@ -1,26 +1,29 @@
 import os
+import os.path
 import subprocess
 import sublime
 import sublime_plugin
 
 
 class SelectInTotalCommanderCommand(sublime_plugin.TextCommand):
+    def set_exe(self, exe):
+        if os.path.exists(exe):
+            print('exe: ', exe)
+            self.exe = exe
+            return True
+        return False    
+
     def __init__(self, view):
         self.view = view
-        self.settings = sublime.load_settings("Open in Total Commander.sublime-settings")
-        env_name = self.settings.get("path_environment_variable")
+        settings = sublime.load_settings("Open in Total Commander.sublime-settings")
+        self.args = settings.get("aruments")
+        env_name = settings.get("path_environment_variable")
         #print('env_name: ', env_name)
-        env = ''
+        if not self.set_exe(os.environ[env_name]):
+            if not self.set_exe(self.settings.get("executable")):
+                if not self.set_exe(self.settings.get("executable2")):
+                    sublime.error_message('No executable found, check Open in Total Commander.sublime-settings!')
 
-        exe = self.settings.get("executable")
-        #print('exe: ', exe)	
-        if env_name:
-            env = os.environ[env_name]
-            print('env: ', env)
-            self.exe = os.path.join(env, exe)
-        else:
-            self.exe = exe
-        self.args = self.settings.get("aruments")
 
     def run(self, edit):
         path = self.view.file_name()
